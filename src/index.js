41ed08d84e541a144ff9088e5016edf2a591e1de
Haseeb <38959887+MHaseebKhalid@users.noreply.github.com>
2024-05-15 16:00:42 +0500

    UID in params

--- src/index.js
+++ src/index.js
@@ -12,9 +12,13 @@ import API from "./ChatbotAPI";
 import "./styles.css";
 import Header from "./components/Header";
 
+import { v4 as uuidv4 } from 'uuid';
+
 function Chatbot() {
   const [messages, setMessages] = useState([]);
+
   const fetchChatAPI = async (text) => {
+    let uid = uuidv4();
     if (text === " ") return;
     try {
       let data = JSON.stringify({
@@ -24,7 +28,7 @@ function Chatbot() {
       let config = {
         method: "post",
         maxBodyLength: Infinity,
-        url: "https://76n9i9hrp9.execute-api.us-east-1.amazonaws.com/genai-app-poc-ApiStage/api/v1/llm/rag",
+        url: `https://76n9i9hrp9.execute-api.us-east-1.amazonaws.com/genai-app-poc-ApiStage/api/v1/llm/rag?UUID=${uid}`,
         headers: {
           authorization:
             "eyJraWQiOiJOc1lNd2VES3pHVTVNTVpITFAwajBkdHNVN1dUZXpOY1pwZVBxTEwrQVhJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJjNDQwMzU1Ny1iNWVkLTRlYWUtOGY2NS1hYjhhYzQzYzZkODEiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9rYzdxcm4wQ2oiLCJjb2duaXRvOnVzZXJuYW1lIjoiYzQ0MDM1NTctYjVlZC00ZWFlLThmNjUtYWI4YWM0M2M2ZDgxIiwib3JpZ2luX2p0aSI6IjU4MjA4ZDI1LWExYmItNGE4Yy05MzAyLTYyY2E1MTBhZjIwYyIsImF1ZCI6IjQxOWFsbzhhNDE1ZGVpN3Myb3JtYWdoYjlwIiwiZXZlbnRfaWQiOiI1OTlkNGQyNi04MTRmLTQyOTgtYTA0OC0xYzg0MjJkNTlkY2EiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY5ODg0MzczMiwiZXhwIjoxNjk4OTMwMTMyLCJpYXQiOjE2OTg4NDM3MzIsImp0aSI6ImRkY2I3MDAzLWM5MmUtNDk0Zi1hYzk5LTBhZjFjNTA3NmM2NCIsImVtYWlsIjoiaGFzc2FuLm5hZGVlbUBuZXRzb2x0ZWNoLmNvbSJ9.h4gAHNGXqoW4HT9-9NDVEF4lrdgO3T4XPLjgGqpqpTdhJ80CgJmcWDA5l-EnG08NUJhwIQfhQILqSgKicvlVZD3VSX-nWfhctxDYAc3ARjJDj4sJa_jnJTKWrUVsnX37LF-ewptUNOq4TVCGeBM7MwZ_vnCOSCPSCdJHRb5_g667cJcnq8XB-Q56hxQu_f6VEgBYm_QpdpjwmjPHUhEQr0ApoF04ZCQPOydX6yncv01YDnH9nra3aCtb2ImcCSnRFxA2PKKTE6ck_IVEDPqC8EaLEFfaA_JycbzfES-XywtE_Mtox9GNE9C67qXYMhI0busGLwTBOG0DQkkZ8SdUKQ",
